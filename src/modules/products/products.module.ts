30145f75948bd2eda558219ee9cfd2a0eb4dcf7e
Juan-Camilo-Sanchez-Echeverri <juancamilosanche65@gmail.com>
2025-03-01 22:58:17 -0500

    refactor: remove product attributes module

--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -2,22 +2,19 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { Product, ProductSchema } from './schemas/product.schema';
-import { ProductsController } from './products.controller';
+import { ProductsController } from './controllers/products.controller';
 import { ProductsService } from './products.service';
 import { OffersModule } from '../offers/offers.module';
-import { ProductAttributesModule } from '../product-attributes/product-attributes.module';
-import { ProductCategoriesModule } from '../product-categories/product-categories.module';
-import { S3Module } from '../s3/s3.module';
+import { CategoriesModule } from '../categories/categories.module';
+import { VariantsController } from './controllers/variants.controller';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
     OffersModule,
-    ProductAttributesModule,
-    ProductCategoriesModule,
-    S3Module,
+    CategoriesModule,
   ],
-  controllers: [ProductsController],
+  controllers: [ProductsController, VariantsController],
   providers: [ProductsService],
   exports: [ProductsService],
 })
