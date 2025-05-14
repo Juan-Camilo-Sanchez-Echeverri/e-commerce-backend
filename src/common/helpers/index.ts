69dfda41b5ac7c2cf2eae2b2b42138c0825236e5
Juan-Camilo-Sanchez-Echeverri <juancamilosanche65@gmail.com>
2025-05-13 19:39:25 -0500

    refactor: replace path-files-upload.helper with file-namer.helper for file naming logic

--- src/common/helpers/index.ts
+++ src/common/helpers/index.ts
@@ -1,6 +1,6 @@
 export * from './date.helper';
 export * from './file-filter.helper';
 export * from './generate-password.helper';
-export * from './path-files-upload.helper';
+export * from './file-namer.helper';
 export * from './user-request.helper';
 export * from './validate-mongo.helper';
