c10f3ed6ba669d9463c8e57f0de5d71e62bcedcf
Juan-Camilo-Sanchez-Echeverri <juancamilosanche65@gmail.com>
2025-05-13 19:51:09 -0500

    refactor: use definite assignment assertion for size and stock properties in SizeStock schema

--- src/modules/products/schemas/size-stock.schema.ts
+++ src/modules/products/schemas/size-stock.schema.ts
@@ -6,10 +6,10 @@ export type SizeStockDocument = SizeStock & Document;
 @Schema()
 export class SizeStock {
   @Prop({ required: true })
-  size: string;
+  size!: string;
 
   @Prop({ required: true })
-  stock: number;
+  stock!: number;
 }
 
 export const SizeStockSchema = SchemaFactory.createForClass(SizeStock);
