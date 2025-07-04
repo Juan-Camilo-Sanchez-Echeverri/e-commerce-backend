e62cf85650dba006cc467833c55e45220f02bd01
Juan-Camilo-Sanchez-Echeverri <juancamilosanche65@gmail.com>
2025-07-03 21:08:27 -0500

    feat: update offers module to create uploads directory dynamically

--- src/modules/offers/offers.module.ts
+++ src/modules/offers/offers.module.ts
@@ -1,18 +1,25 @@
+import { mkdir } from 'fs/promises';
+
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+
+import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 
 import { OffersController } from './offers.controller';
 import { OffersService } from './offers.service';
 import { Offer, OfferSchema } from './schemas/offer.schema';
-import { MulterModule } from '@nestjs/platform-express';
-import { diskStorage } from 'multer';
 import { fileNamer } from '../../common/helpers';
 
 @Module({
   imports: [
     MulterModule.register({
       storage: diskStorage({
-        destination: './uploads/offers',
+        destination: async (_req, _file, cb) => {
+          const UPLOADS_DIR = './uploads/offers';
+          await mkdir(UPLOADS_DIR, { recursive: true });
+          cb(null, UPLOADS_DIR);
+        },
         filename: fileNamer,
       }),
     }),
