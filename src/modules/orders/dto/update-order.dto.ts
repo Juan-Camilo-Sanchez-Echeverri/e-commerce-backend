2c392ab58086a644e9000657129f26d0a64b1e06
Juan-Camilo-Sanchez-Echeverri <juancamilosanche65@gmail.com>
2025-07-07 19:49:25 -0500

    feat: update UpdateOrderDto to include shippingAddress and status validation, and expand OrderStatus enum with additional states

--- src/modules/orders/dto/update-order.dto.ts
+++ src/modules/orders/dto/update-order.dto.ts
@@ -1,5 +1,14 @@
-import { PartialType } from '@nestjs/swagger';
+import { PartialType, PickType } from '@nestjs/mapped-types';
+
+import { IsEnum, IsOptional } from 'class-validator';
 
 import { CreateOrderDto } from './create-order.dto';
+import { OrderStatus } from '../enums/order-status.enum';
 
-export class UpdateOrderDto extends PartialType(CreateOrderDto) {}
+export class UpdateOrderDto extends PartialType(
+  PickType(CreateOrderDto, ['shippingAddress'] as const),
+) {
+  @IsOptional()
+  @IsEnum(OrderStatus)
+  status?: OrderStatus;
+}
